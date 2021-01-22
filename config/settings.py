90609fc0c997c9d47e97e196226cf7f953bbf90b
Jerome Brown <jaebrownjr@gmail.com>
2021-01-21 22:07:41 -0700

    heroku master

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&h&(bvh+=8kn$s91y189#1-@fi+$@8^se4$du1f_32srp8$e^y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
